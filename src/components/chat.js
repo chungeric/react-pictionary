dffa003f937e955e769ea23feda9fed7c6010030
Eric Chung <rchung.eric@gmail.com>
2018-01-24 17:38:55 +1100

    removed jquery dependencies

--- src/components/chat.js
+++ src/components/chat.js
@@ -5,68 +5,72 @@ import { bindActionCreators } from 'redux';
 import { addPlayer, removePlayer, updatePlayers } from '../actions';
 
 class Chat extends Component {
-
   constructor(props) {
     super(props);
     this.onMessageSubmit = this.onMessageSubmit.bind(this);
+    this.state = {
+      sessionId: null
+    };
   }
 
   componentDidMount() {
+
+    // SELF CONNECTED
     this.props.socket.on('connect', () => {
       const sessionId = this.props.socket.id.slice(0,8);
       this.setState({ sessionId });
-      $(".message:first-of-type").append(
-        `<p>
-          @: Welcome to the chat <strong><span style="color: red;">&lt;${sessionId}&gt;</span></strong>.
-          Type something below and press enter to start chatting away!
-        </p>`
-      );
 
-      // add to player count in application state
+      // add to my player count in application state
       this.props.addPlayer();
-      console.log(this.props.numPlayers);
-
-      $(".message:first-of-type").append(
-        `<p class="online">
-          <strong><span style="color: #666;">${this.props.numPlayers}</span></strong> players currently online.
-        </p>`
-      );
-
     });
 
+
+    // NEW USER (NOT SELF) CONNECTED
     this.props.socket.on('connected', ({ sessionId }) => {
-      $(".message:last-of-type").after(
-        `<div class='message'><p>${sessionId.slice(0,8)} connected.</p></div>`
+      this.getLastMsgNode().insertAdjacentHTML('afterend',
+        `<div class='message'><p>${ sessionId.slice(0,8) } connected.</p></div>`
       );
+
+      // add to my player count in application state
       this.props.addPlayer();
-      console.log(this.props.numPlayers);
+
+      // send up to date number of players to everyone, so everyone is up to date
       this.props.socket.emit('share-numplayers', ({ numPlayers: this.props.numPlayers }));
 
+      // updates number of players online displayed
       this.updateNumOnline();
     });
 
+
+    // UPDATE MY SOCKET'S NUMPLAYERS STATE
     this.props.socket.on('update-numplayers', ({ numPlayers }) => {
       this.props.updatePlayers(numPlayers);
-      console.log(this.props.numPlayers);
 
+      // updates number of players online displayed
       this.updateNumOnline();
     });
 
+
+    // MESSAGE SENT BY ANOTHER PLAYER SHOULD BE DISPLAYED
     this.props.socket.on('message-sent', ({ message, sessionId }) => {
-      $(".message:last-of-type").after(
+      this.getLastMsgNode().insertAdjacentHTML('afterend',
         `<div class='message'>
-          <p><strong><span style="color: red;">&lt;${sessionId.slice(0,8)}&gt;</span></strong>: ${message}</p>
+          <p><strong><span style="color: red;">&lt;${ sessionId.slice(0,8) }&gt;</span></strong>: ${ message }</p>
         </div>`
       );
     });
 
-    this.props.socket.on('disconnected', ({ sessionId }) => {
-      $(".message:last-of-type").after(
-        `<div class='message'><p>${sessionId.slice(0,8)} disconnected.</p></div>`
-      );
-      this.props.removePlayer();
-      console.log(this.props.numPlayers);
 
+    // PLAYER DISCONNECTED
+    this.props.socket.on('disconnected', ({ sessionId }) => {
+      this.getLastMsgNode().insertAdjacentHTML('afterend',
+        `<div class='message'><p>${ sessionId.slice(0,8) } disconnected.</p></div>`
+      );
+
+      // deducts from player count in application state
+      this.props.removePlayer();
+
+      // updates number of players online displayed
       this.updateNumOnline();
     });
   }
@@ -75,35 +79,51 @@ class Chat extends Component {
     if (event.key == 'Enter') {
       let message = event.target.value;
       if (message !== '') {
-        $(".message:last-of-type").after(
+        this.getLastMsgNode().insertAdjacentHTML('afterend',
           `<div class='message'>
-            <p><strong><span style="color: red;">&lt;${this.state.sessionId.slice(0,8)}&gt;</span></strong>: ${message}</p>
+            <p><strong><span style="color: red;">&lt;${ this.state.sessionId }&gt;</span></strong>: ${message}</p>
           </div>`
         );
         event.target.value = '';
 
         // trigger message-sent event
-        // this.socket.emit('message-sent', { message, sessionId: this.state.sessionId });
         this.props.socket.emit('message-sent', { message, sessionId: this.state.sessionId });
       }
     }
   }
 
+  getLastMsgNode() {
+    let msgNodes = document.querySelectorAll('.message');
+    return msgNodes[msgNodes.length - 1];
+  }
+
   updateNumOnline() {
-    $(".message:first-of-type > .online").replaceWith(
-      `<p class="online">
-        <strong><span style="color: #666;">${this.props.numPlayers}</span></strong> players currently online.
-      </p>`
-    );
+    document.querySelector(".message > .online").innerHTML = `<p class="online">
+      <strong><span style="color: #666;">${ this.props.numPlayers }</span></strong> players currently online.
+    </p>`;
   }
 
   render() {
+    const strongRed = {
+      color: 'red',
+      fontWeight: 'bold'
+    };
+    const strongGrey = {
+      color: '#666',
+      fontWeight: 'bold'
+    };
     return (
       <div className="chat">
         <div className="messages-wrapper">
           <div className="messages">
             <div className="message">
-
+              <p className="welcome">
+                @: Welcome to the chat <strong><span style={ strongRed }>&lt;{ this.state.sessionId ? this.state.sessionId : '' }&gt;</span></strong>.
+                Type something below and press enter to start chatting away!
+              </p>
+              <p className="online">
+                <span style={ strongGrey }>{ this.props ? this.props.numPlayers : '' }</span> players currently online.
+              </p>
             </div>
           </div>
         </div>
